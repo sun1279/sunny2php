2ed14ced7f5dffbafc9c8df8abbefe4bfc468709
You2php <34645738+You2php@users.noreply.github.com>
2018-01-24 18:02:53 +0800

    修复了安装页乱码

--- install.php
+++ install.php
@@ -2,6 +2,7 @@
 <html lang="zh-cn">
 <head>
     <title>You2PHP安装！</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="https://wangxiaoqing123.coding.me/hellocss.coding.me/pintuer.css">
